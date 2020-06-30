b249ae7911a84872713801cb27cffd5683133429
xinyuehtx <xinyuehtx@xinyuehtxs-MacBook-Pro.local>
2020-06-30 21:23:08 +0800

    添加button样式

--- tomato-timer/src/App.js
+++ tomato-timer/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Timer from './Timer/index'
 
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <Timer className="timer"/>
       <div className="main-button">开始工作</div>
     </div>
   );
