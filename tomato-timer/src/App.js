50a2628aa7fae5a59d9e9c8c6129cbb3ef766078
xinyuehtx <xinyuehtx@xinyuehtxs-MacBook-Pro.local>
2020-06-30 21:45:01 +0800

    添加按键状态

--- tomato-timer/src/App.js
+++ tomato-timer/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Timer from './Timer/index'
 
 function App() {
+  const [run, setRun] = useState(false);
+
+  const handleClick=()=>{
+    setRun(!run);
+  }
+
   return (
     <div className="App">
-      <Timer/>
-      <div className="main-button">开始工作</div>
+      <Timer />
+      <div className="main-button" onClick={handleClick}>{
+        run
+          ? '休息一下'
+          : '开始工作'
+      }</div>
     </div>
   );
 }
