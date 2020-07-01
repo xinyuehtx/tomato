dddc348d8f535bf2c3180d11fefe7fe84771fa1e
xinyuehtx <xinyuehtx@xinyuehtxs-MacBook-Pro.local>
2020-07-01 22:08:39 +0800

    调整样式，确保设置按钮位置一致

--- tomato-timer/src/App.js
+++ tomato-timer/src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [initTime, setInitTime] = useState(1);
   const [workTime, setWorkTime] = useState(25);
   const [restTime, setRestTime] = useState(5);
+  const [showSetting, setShowSetting] = useState(false);
 
   useEffect(() => {
     if (run === runStates.INIT) {
@@ -51,31 +52,50 @@ function App() {
   }
 
   const handleSettingButtonClick = () => {
-    setRun(runStates.INIT);
-    setWorkTime(10);
-    setRestTime(2);
+    if (showSetting) {
+      setShowSetting(false);
+      setWorkTime(10);
+      setRestTime(2);
+    }
+    else {
+      setShowSetting(true);
+      setRun(runStates.INIT);
+    }
   }
 
 
   return (
     <div className="App">
-      <Timer run={run} initTime={initTime} onTimeout={handleTimeout} />
       {
-        inWork
-          ? <div className="main-button" onClick={handleClick}>{
-            run === runStates.INIT
-              ? '开始工作'
-              : run === runStates.RUNNING
-                ? '休息一下'
-                : '继续工作'
-          }</div>
-          : <div className="main-button" onClick={handleRestClick}>{
-            run === runStates.INIT
-              ? '开始休息'
-              : '结束休息'
-          }</div>
+        showSetting
+          ? <div className="setting-container">设置</div>
+          : <>
+            <Timer run={run} initTime={initTime} onTimeout={handleTimeout} />
+            {
+              inWork
+                ? <div className="main-button" onClick={handleClick}>{
+                  run === runStates.INIT
+                    ? '开始工作'
+                    : run === runStates.RUNNING
+                      ? '休息一下'
+                      : '继续工作'
+                }</div>
+                : <div className="main-button" onClick={handleRestClick}>{
+                  run === runStates.INIT
+                    ? '开始休息'
+                    : '结束休息'
+                }</div>
+            }
+          </>
       }
-      <div className="setting-button" onClick={handleSettingButtonClick}>设置时间</div>
+
+      <div className="setting-button" onClick={handleSettingButtonClick}>
+        {
+          showSetting
+            ? '完成设置'
+            : '设置时间'
+        }
+      </div>
     </div>
   );
 }
