adae0c02e426a852db1ccfdaf4ad11f2b3aa1bef
xinyuehtx <xinyuehtx@xinyuehtxs-MacBook-Pro.local>
2020-07-02 22:31:58 +0800

    添加设置值样式

--- tomato-timer/src/Setting/index.js
+++ tomato-timer/src/Setting/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
+import TimeSetter from '../TimeSetter/index';
 
 function Setting(props) {
     return (
         <div className="setting-container">
-            设置
+            <TimeSetter initTime={25} title='工作时间' />
+            <TimeSetter initTime={5} title='休息时间' />
         </div>
     );
 }
--- tomato-timer/src/TimeSetter/index.js
+++ tomato-timer/src/TimeSetter/index.js
@@ -1,12 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import './style.css';
 
-function Setting(props) {
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'addOne': return state + 1;
+        case 'addTen': return state + 10;
+        default: throw new Error('Unexpected action');
+    }
+}
+
+function TimeSetter(props) {
+
+    const { initTime, title, dispatch } = props;
+
+    const [currentTime, currentTimeDispatch] = useReducer(reducer, initTime);
+
+    const high = Math.floor(currentTime / 10);
+    const low = currentTime % 10;
+
     return (
-        <div className="setting-container">
-            设置
+        <div className="timesetter-container">
+            <div className="timersetter-title">{title}</div>
+            <div className="timesetter-number-container">
+                {high}
+            </div>
+            <div className="timesetter-number-container">
+                {low}
+            </div>
+            <div className="timesetter-unit">min</div>
         </div>
     );
 }
 
-export default Setting;
+export default TimeSetter;
