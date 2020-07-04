d0b8b08c5ae6822c12101b3fdb8c7affdbd613df
xinyuehtx <xinyuehtx@xinyuehtxs-MacBook-Pro.local>
2020-07-04 18:28:37 +0800

    添加倒计时的进度条

--- tomato-timer/src/Timer/index.js
+++ tomato-timer/src/Timer/index.js
@@ -36,10 +36,28 @@ function Timer(props) {
 
     const [minutes, seconds] = formatTime(remainTime);
 
+    const progress = (1 - (remainTime - 1) / (initTime * 60)) * 3.14 * 98;
+
     return (
         <div className="root">
-            <div className="time">{`${minutes}:${seconds}`}</div>
-        </div>
+            <svg className="timer-progress" viewBox='0 0 100 100'>
+                <circle cx='50' cy='50' r='48' stroke='#ccc'
+                    strokeWidth='2' fill='transparent' />
+                <path className={`timer-stroke ${run === runStates.RUNNING && 'timer-stroke-running'}`}
+                    d='M 50,50
+                        m 0,-48 
+                        a 48,48 0 1 0 0,96
+                        a 48,48 0 1 0 0,-96'
+                    strokeWidth='2' strokeLinecap='round'
+                    fill='transparent'
+                    stroke='red'
+                    style={{ 'stroke-dashoffset': progress }} />
+            </svg>
+            <div className="timer-view">
+                <div className="time">{`${minutes}:${seconds}`}</div>
+            </div>
+
+        </div >
     );
 }
 
